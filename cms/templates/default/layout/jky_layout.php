a6eaf13c87f23cd222dcc8afa8bf699a30ef5662
fengxin5571 <502251245@qq.com>
2018-02-07 16:44:24 +0800

    2018 2 7 冯鑫修改移动端跳转入口

--- cms/templates/default/layout/jky_layout.php
+++ cms/templates/default/layout/jky_layout.php
@@ -1,4 +1,32 @@
 <?php defined('InShopNC') or exit('Access Invalid!');?>
+<?php 
+$wapurl = WAP_SITE_URL;
+$agent = $_SERVER['HTTP_USER_AGENT'];
+if(strpos($agent,"comFront") || strpos($agent,"iPhone") || strpos($agent,"MIDP-2.0") || strpos($agent,"Opera Mini") || strpos($agent,"UCWEB") || strpos($agent,"Android") || strpos($agent,"Windows CE") || strpos($agent,"SymbianOS")){
+    global $config;
+    if(!empty($config['wap_site_url'])){
+        $url = $config['wap_site_url'];
+        switch ($_GET['act']){
+            case 'goods':
+                $url .= '/tmpl/product_detail.html?goods_id=' . $_GET['goods_id'];
+                break;
+            case 'store_list':
+                $url .= '/shop.html';
+                break;
+            case 'show_store':
+                $url .= '/tmpl/store.html?store_id=' . $_GET['store_id'];
+                break;
+            case 'article':
+                $url .= '/tmpl/article_detail_wap.html?article_id=' . $_GET['article_id'];
+                break;
+        }
+    } else {
+        $header("Location:$wapurl");
+    }
+    header('Location:' . "http://m.sxtaichang.com/");
+    exit();
+}
+?>
 <?php if ($output['index_sign']=="index") {?>
 <?php require CMS_BASE_TPL_PATH.'/layout/tc_top.php';}else{?>
 <?php require CMS_BASE_TPL_PATH.'/layout/jky_top.php';?>
